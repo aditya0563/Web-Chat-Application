fa2acfde1d3502ef89ae7065fbb1e1e1ea93b2aa
Aditya Thakur <aditya@example.com>
2026-03-17 01:44:00 +0530

    add socket setup

---
+++ backend/socket/socket.js
@@ -0,0 +1,36 @@
+import { Server } from "socket.io";
+import http from "http";
+import express from "express";
+
+const app = express();
+
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: [process.env.FRONTEND_URL || "http://localhost:5173"],
+    methods: ["GET", "POST"],
+  },
+});
+
+export const getReceiverSocketId = (receiverId) => {
+  return userSocketMap[receiverId];
+}
+
+const userSocketMap = {};
+
+io.on("connection", (socket) => {
+  const userId = socket.handshake.query.userId;
+  if (userId ) {
+    userSocketMap[userId] = socket.id;
+  }
+
+  io.emit("getOnlineUsers", Object.keys(userSocketMap));
+
+  socket.on("disconnect", () => {
+    console.log("User Disconnected", socket.id);
+    delete userSocketMap[userId];
+    io.emit("getOnlineUsers", Object.keys(userSocketMap));
+  });
+});
+
+export { app, io, server };
